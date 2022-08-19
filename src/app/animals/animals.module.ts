ea935b27375ec64082c4768cc465a6f67f469688
Gabriel <biel_borel@hotmail.com>
2022-08-19 20:04:41 -0300

    Animals module created

---
+++ src/app/animals/animals.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AnimalsRoutingModule } from './animals-routing.module';
+import { AnimalsListComponent } from './animals-list/animals-list.component';
+
+@NgModule({
+  declarations: [
+    AnimalsListComponent
+  ],
+  imports: [CommonModule, AnimalsRoutingModule],
+})
+export class AnimalsModule {}
