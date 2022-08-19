82137a68efe1057a851035743c0c5d9c5ced0b22
Gabriel <biel_borel@hotmail.com>
2022-08-19 20:07:06 -0300

    Added HttpClientModule to the App modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MessageComponent } from './components/message/message.component';
 
 @NgModule({
   declarations: [AppComponent],
