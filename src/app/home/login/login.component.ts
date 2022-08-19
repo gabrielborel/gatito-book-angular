84ed75f7f7d7b10c85f2ccc4be6ce904cfec7d9d
Gabriel <biel_borel@hotmail.com>
2022-08-19 20:08:01 -0300

    Added MessageComponent to LoginHtml/TS

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/authentication/authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  user = '';
+  password = '';
 
-  constructor() { }
+  constructor(
+    private authService: AuthenticationService,
+    private routerService: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    this.authService.authenticate(this.user, this.password).subscribe(
+      (res) => {
+        console.log(`Succesfully authenticated!`);
+        console.log(res);
+        this.routerService.navigateByUrl('animals');
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
   }
-
 }
